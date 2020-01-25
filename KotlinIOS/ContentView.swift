ddb211b0ad0c018cba683bb95a691df22aa849b6
BRIJQ Brian Oh <brijq91@gmail.com>
2020-01-25 23:18:41 +0800

    Initial Commit

---
+++ KotlinIOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KotlinIOS
+//
+//  Created by Brian Oh on 25/1/20.
+//  Copyright © 2020 Brian Oh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
